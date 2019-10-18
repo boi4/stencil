96992815271d74e85857e88dbcd5361283e13a7d
Jan Fecht <jan.fecht@tum.de>
2019-10-19 00:24:57 +0100

    Add unecessary min macro

--- stencil.h
+++ stencil.h
@@ -6,7 +6,8 @@
 // Define output file name
 #define OUTPUT_FILE "stencil.pgm"
 #define WHITE_FLOAT 100.0f
-#define BORDER_FIELD_ALIGNMENT 0x80 // should be at least 16 for vectorization
+#define BORDER_FIELD_ALIGNMENT 0x80 // should be at least 16 for vectorization and not greater than 0x1000
+#define MIN(a,b) (((a)<(b))?(a):(b))
 
 
 struct float_ptr_pair {
